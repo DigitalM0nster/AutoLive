d2515b6d022208100048f89fae461f172cfade9f
DigitalM0nster <valentin.webs@gmail.com>
2025-08-23 04:38:00 +0300

    upd

--- src/app/admin/departments/local_components/DepartmentLogsComponent.tsx
+++ src/app/admin/departments/local_components/DepartmentLogsComponent.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from "./styles.module.scss";
 import Pagination from "@/components/ui/pagination/Pagination";
 import CustomSelect from "@/components/ui/customSelect/CustomSelect";
