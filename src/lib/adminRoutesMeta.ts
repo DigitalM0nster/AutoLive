139629c2d6607e72fa273cf3cf3238ec3ec8bddb
DigitalM0nster <valentin.webs@gmail.com>
2025-10-30 05:37:59 +0300

    update colors

--- src/lib/adminRoutesMeta.ts
+++ src/lib/adminRoutesMeta.ts
@@ -13,39 +13,40 @@ export const adminRoutesMeta: Record<string, AdminRouteMeta> = {
 	departments: {
 		label: "Отделы",
 		icon: Building2,
-		bg: "indigo",
+		bg: "blue",
 	},
 	users: {
 		label: "Пользователи",
 		icon: Users,
-		bg: "indigo",
+		bg: "blue",
+	},
+	categories: {
+		label: "Категории",
+		icon: ListOrdered,
+		bg: "blue",
+	},
+	"product-management": {
+		label: "Управление товарами",
+		icon: Package,
+		description: "Товары, Комплекты ТО",
+		bg: "green",
+	},
+	bookings: {
+		label: "Записи",
+		icon: ClipboardList,
+		bg: "green",
 	},
 	orders: {
 		label: "Заказы",
 		icon: ShoppingCart,
 		bg: "green",
 	},
-	bookings: {
-		label: "Записи",
-		icon: ClipboardList,
-		bg: "purple",
-	},
-	categories: {
-		label: "Категории",
-		icon: ListOrdered,
-		bg: "orange",
-	},
 	content: {
 		label: "Контент сайта",
 		icon: FileText,
-		bg: "pink",
+		bg: "orange",
 	},
-	"product-management": {
-		label: "Управление товарами",
-		icon: Package,
-		description: "Товары, Комплекты ТО",
-		bg: "blue",
-	},
+
 	profile: { label: "Редактирование профиля" },
 	products: { label: "Список товаров" },
 	kits: { label: "Комплекты ТО" },
